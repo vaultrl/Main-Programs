cb87d7d9b47f5cf7dd338ea067a958d01be83f98
vaultrl <73655721+vaultrl@users.noreply.github.com>
2021-04-29 23:01:41 -0600

    Add files via upload

---
+++ Fitness_Site/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Jacob Carr   
+    File Name: script.js
+    Date: 11/30/2020
+*/
+//jQwery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
